713f04163b38277cc30971b1a06e26f355a186e4
ErtaylanKayacan <101421107+ErtaylanKayacan@users.noreply.github.com>
2022-11-05 20:06:47 +0300

    Add files via upload

---
+++ JavaÖdev_3/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product = new Product(4,"Laptop","Asus Laptop",3000,3,"Siyah");
+//		product.setName("Laptop");
+//		product.setId(2);
+//		product.setPrice(18999);
+//		product.setDescription("Asus Laptop");
+//		product.setStockAmount(1);
+//		product.setRenk("Siyah");
+		
+		System.out.println(product.getKod());
+		
+	}	
+
+}
